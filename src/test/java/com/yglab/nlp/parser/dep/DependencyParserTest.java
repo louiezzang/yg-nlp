5f227dc2ff3815de88bdca3ed3e08b56999186ca
louiezzang <louiezzang@gmail.com>
2014-03-21 10:06:08 +0900

    Add @Ignore to DependencyParserTest

--- src/test/java/com/yglab/nlp/parser/dep/DependencyParserTest.java
+++ src/test/java/com/yglab/nlp/parser/dep/DependencyParserTest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.yglab.nlp.model.AbstractModel;
@@ -17,7 +18,7 @@ import com.yglab.nlp.parser.io.CONLLReader;
  * 
  * @author Younggue Bae
  */
-//@Ignore
+@Ignore
 public class DependencyParserTest {
 	
 	private static DefaultDependencyFeatureGenerator featureGenerator;
