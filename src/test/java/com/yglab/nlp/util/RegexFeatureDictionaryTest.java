74ea7c9d47e0f136dcb6953f392cc0f0f4dae140
louiezzang <louiezzang@gmail.com>
2014-04-06 13:53:03 +0900

    Update ko-regex-feature-unit.dic

--- src/test/java/com/yglab/nlp/util/RegexFeatureDictionaryTest.java
+++ src/test/java/com/yglab/nlp/util/RegexFeatureDictionaryTest.java
@@ -20,7 +20,7 @@ public class RegexFeatureDictionaryTest {
 		RegexFeatureDictionary featureDic = new RegexFeatureDictionary(
 				//"/lang/ko/ko-regex-feature.dic",
 				"/lang/ko/ko-regex-feature-unit.dic");
-		String text = "!!!! 20~40대까지 12월에서 내년 2월까지 20~30살까지";
+		String text = "!!!! 20~40대까지 12월에서 내년 2월까지 20~30살까지 강모 씨(47)는 서모 씨(31·배관공)는 6만∼7만원을 1백만원은 이모씨(59·여)는 1991/01/15";
 		String[] features = featureDic.getFeatures(text);
 
 		for (String feature : features) {
